0d3a343a32c0f130cfb1f5bb2760254c890bef24
邢浩东 <975115611@qq.com>
2024-12-24 10:31:22 +0800

    chore: 更新 .gitignore 并添加 login 日志输出
    - 更新 .gitignore 文件，将 .env 相关配置文件的忽略规则注释掉
    - 在 auth.controller.ts 中添加 login 方法的日志输出，便于调试

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,6 +11,7 @@ export class AuthController {
     @Post('login')
     async login(@Body() body: { username: string; password: string }) {
         const user = await this.authService.validateUser(body.username, body.password);
+        console.log(user, 'user');
         if (!user) {
             throw new HttpException('未找到用户', HttpStatus.NOT_FOUND);
         }
