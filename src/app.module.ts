c00613101ae51671db7758d8d8162b794ef98087
xinghaodong <975115611@qq.com>
2025-06-19 20:21:33 +0800

    perf: 关闭数据库同步功能
    - 将 TypeORM 的 synchronize 选项从 true 修改为 false
    - 此修改可提高生产环境的稳定性和性能

--- src/app.module.ts
+++ src/app.module.ts
@@ -51,7 +51,7 @@ import { AiModule } from './ai-com/ai-com.module';
                 username: configService.get('DB_USERNAME'), // 用户名
                 password: configService.get('DB_PASSWORD'), // 密码
                 database: configService.get('DB_DATABASE'), //数据库名
-                synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
+                synchronize: false, //根据实体自动创建数据库表， 生产环境建议关闭
             }),
         }),
         InternalusersModule,
